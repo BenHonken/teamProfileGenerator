b12f93980f5afc3329210532b0d20124a4811b08
Ben Honken <honkenb@gmail.com>
2019-11-24 14:08:33 -0600

    removed some unecessary code that was commented out.

--- index.js
+++ index.js
@@ -96,22 +96,6 @@ async function inputSchool() {
     );
 }
 
-// function generateHTML(answers, gitInfo, stars) {
-//     return `
-//   <!DOCTYPE html>
-//   <html lang="en">
-//   <head>
-//     <meta charset="UTF-8">
-//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-//     <title>Document</title>
-//   </head>
-//   <body style="background-color: white">
-    
-//   </body>
-//   </html>`;
-//   }
-
 async function init() {
     console.log("hi");
     try {
